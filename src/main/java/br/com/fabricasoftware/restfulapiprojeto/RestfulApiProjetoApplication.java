62cec93ec1116a242da04800d438adc924cc45f1
Alefe Patrick Dias Ramos <CIANDT\alefep@lnb035864cps.ciandt.global>
2022-05-04 16:57:40 -0400

    adicionando novos arquivos

---
+++ src/main/java/br/com/fabricasoftware/restfulapiprojeto/RestfulApiProjetoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fabricasoftware.restfulapiprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fabricasoftware/restfulapiprojeto/RestfulApiProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabricasoftware.restfulapiprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
