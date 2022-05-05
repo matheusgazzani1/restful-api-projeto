87ae0f2c57ba1d64915042a5d8e5a0bf7da1290f
aleferamos <alefepdias@gmail.com>
2022-05-04 20:29:38 -0400

    teste

--- src/main/java/br/com/fabricasoftware/restfulapiprojeto/controlers/UsuarioController.java
+++ src/main/java/br/com/fabricasoftware/restfulapiprojeto/controlers/UsuarioController.java
@@ -5,10 +5,11 @@ import br.com.fabricasoftware.restfulapiprojeto.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("usuario")
@@ -17,8 +18,8 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-    @GetMapping
-    public ResponseEntity<List<Usuario>> listar(){
-        return ResponseEntity.ok(usuarioService.list());
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Usuario>> listar(@PathVariable(name = "id") Long id){
+        return ResponseEntity.ok(usuarioService.list(id));
     }
 }
--- src/main/java/br/com/fabricasoftware/restfulapiprojeto/services/UsuarioService.java
+++ src/main/java/br/com/fabricasoftware/restfulapiprojeto/services/UsuarioService.java
@@ -5,7 +5,7 @@ import br.com.fabricasoftware.restfulapiprojeto.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -13,7 +13,7 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    public List<Usuario> list(){
-        return usuarioRepository.findAll();
+    public Optional<Usuario> list(Long id){
+        return usuarioRepository.findById(id);
     }
 }
